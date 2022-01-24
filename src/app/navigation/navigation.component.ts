a2dc49b42c07284682604150a0b57c765e53e3c3
jordynandmartin <ashley-laird@hotmail.com>
2022-01-23 21:12:40 -0700

    Added sticky navigation bar

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+
+export class NavigationComponent implements OnInit , AfterViewInit{
+  @ViewChild('navigationBar') menuElement!: ElementRef;
+
+  isSticky : boolean = false;
+  menuPosition!: number;
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+    this.menuPosition = this.menuElement.nativeElement.offsetTop;
+    console.log(this.menuElement);
+  }
+
   ngOnInit(): void {
   }
+
+  @HostListener('window:scroll', ['$event'])
+    handleScroll(){
+      const windowScroll = window.pageYOffset;
+      if(windowScroll >= this.menuPosition){
+          this.isSticky = true;
+      } else {
+          this.isSticky = false;
+      }
+}
 
 }
